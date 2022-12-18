5356b6f3f2e135f75542a3551c3461dd3056d77c
Vaibhav Dhiman <vars7899@gmail.com>
2022-12-18 00:31:32 -0800

    Dev: Dynamic stripe data

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -5,17 +5,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // <----------------- Stripe endpoints start ------------------>
 const createStripePaymentIntent = asyncHandler(async (req, res) => {
+  const { amount, currency, metadata } = req.body;
+  console.log("done");
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 5000, // in cents
-    currency: "usd",
-    metadata: {
-      movieId: "63914e6ceab81ed6c0cde4fa",
-      hall: Math.random() * 9 + 1,
-      seats: "11,12,13",
-      theatreId: "639160dcdcf51848245b9aef",
-      userId: "63903cb461342d78a860fab3",
-      ticketDate: 1567562058,
-    },
+    amount,
+    currency,
+    metadata,
   });
   res.status(200).json({
     clientSecret: paymentIntent.client_secret,
